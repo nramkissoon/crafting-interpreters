9d4367f5d0ba49118eabe686ddb1e3658509e1e7
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-01-05 16:59:40 -0500

    vm

--- clox/debug.c
+++ clox/debug.c
@@ -20,6 +20,7 @@ static int constantInstruction(const char *name, Chunk *chunk,
   printf("%-16s %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
   printf("'\n");
+  return offset + 2;
 }
 
 static int simpleInstruction(const char *name, int offset)
@@ -45,8 +46,16 @@ int disassembleInstruction(Chunk *chunk, int offset)
   {
   case OP_CONSTANT:
     return constantInstruction("OP_CONSTANT", chunk, offset);
-    // return +2 because constant instruction is opcode + offset in value array (2 values)
-    return offset + 2;
+  case OP_ADD:
+    return simpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:
+    return simpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:
+    return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return simpleInstruction("OP_DIVIDE", offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
