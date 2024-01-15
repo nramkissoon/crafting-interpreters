5cd83cd15fa300761e79a33259016b047f1609be
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-01-14 22:41:44 -0500

    function wip

--- clox/compiler.c
+++ clox/compiler.c
@@ -44,7 +44,15 @@ typedef struct {
   int depth;
 } Local;
 
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT,
+} FunctionType;
+
 typedef struct {
+  ObjFunction* function;
+  FunctionType type;
+  
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
@@ -52,10 +60,9 @@ typedef struct {
 
 Parser parser;
 Compiler* current = NULL;
-Chunk* compilingChunk;
 
 static Chunk* currentChunk() {
-  return compilingChunk; 
+  return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) {
@@ -170,19 +177,30 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currentChunk(), function->name != NULL ? function->name->chars : "<script>");
   }
 #endif
+
+  return function;
 }
 
 static void beginScope() {
@@ -431,6 +449,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0) return;
   current->locals[current->localCount - 1].depth =
       current->scopeDepth;
 }
@@ -467,6 +486,13 @@ static void block() {
   }
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void varDeclaration() {
@@ -599,6 +625,9 @@ static void synchronize() {
 }
 
 static void declaration() {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  }
   if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
@@ -626,11 +655,10 @@ static void statement() {
   }
 }
 
-bool compile(const char* source, Chunk* chunk) {
+bool compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -642,7 +670,7 @@ bool compile(const char* source, Chunk* chunk) {
     declaration();
   }
 
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction* function = endCompiler();
+  return parser.hadError ? NULL : function;
 }
 
--- clox/vm.h
+++ clox/vm.h
@@ -1,16 +1,25 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAMES_MAX 64
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
+
+// A CallFrame represents a single ongoing function call.
+typedef struct {
+  ObjFunction* function;
+  uint8_t* ip;
+  Value* slots; //points into the VM’s value stack at the first slot that this function can use
+} CallFrame;
 
 typedef struct
 {
-  Chunk *chunk;
-  uint8_t *ip; // instruction pointer
+  CallFrame frames[FRAMES_MAX];
+  int frameCount;
+
   Value stack[STACK_MAX];
   Value *stackTop;
   Table globals;
