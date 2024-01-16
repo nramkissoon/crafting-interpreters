e19527040c92306aa75d17dba4c4ebe4ecc437c2
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2024-01-16 14:50:59 -0500

    gc

--- clox/chunk.c
+++ clox/chunk.c
@@ -2,6 +2,7 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "vm.h"
 
 void initChunk(Chunk *chunk)
 {
@@ -38,7 +39,9 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line)
 
 int addConstant(Chunk *chunk, Value value)
 {
+  push(value);
   writeValueArray(&chunk->constants, value);
+  pop();
   // return the index of the constant in the value array
   return chunk->constants.count - 1;
 }
--- clox/compiler.h
+++ clox/compiler.h
@@ -5,5 +5,6 @@
 #include "vm.h"
 
 bool compile(const char* source);
+void markCompilerRoots();
 
 #endif
